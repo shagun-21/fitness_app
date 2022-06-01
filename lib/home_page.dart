6a0f73cad7231e99b8e6481f22e12abe02c98ea6
Isha pawar <pawrani159@gmail.com>
2022-06-01 19:45:03 +0530

    completed till the blue box, till 38:30

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,56 +15,178 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: color.AppColor.homePageBackground,
       body: Container(
-        padding: EdgeInsets.only(top: 70,left: 30,right: 30),
+        padding: EdgeInsets.only(top: 70, left: 30, right: 30),
         //Main Column of Home PAge
         child: Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text("Training",
-                    style: TextStyle(
+                Text(
+                  "Training",
+                  style: TextStyle(
                       color: color.AppColor.homePageTitle,
                       fontSize: 30,
-                      fontWeight: FontWeight.w700
-                    ),
-                    ),
-                    Expanded(child: Container()),
-                    Icon(Icons.arrow_back_ios,
-                    size: 20,
-                    color: color.AppColor.homePageIcons,),
-                    SizedBox(width: 10,),
-                    Icon(Icons.calendar_today_outlined,
-                    size: 20,
-                    color: color.AppColor.homePageIcons,),
-                    SizedBox(width: 14,),
-                    Icon(Icons.arrow_forward_ios,
-                    size: 20,color:color.AppColor.homePageIcons ,)
-                  ],
+                      fontWeight: FontWeight.w700),
                 ),
-                SizedBox(height: 30,),
-                Row(
-                  children: [
-                    Text("Your program",
-                    style: TextStyle(
+                Expanded(child: Container()),
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                ),
+                SizedBox(
+                  width: 14,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Your program",
+                  style: TextStyle(
                       fontSize: 20,
                       color: color.AppColor.homePageSubtitle,
-                      fontWeight: FontWeight.w700
-                    ),),
-                    Expanded(child: Container()),
-                    Text("Details",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: color.AppColor.homePageDetail,
-                    ),),
-
-                    SizedBox(width: 5,),
-                    Icon(Icons.arrow_forward,
-                    size: 20,
-                    color: color.AppColor.homePageIcons,)
-                  ],
+                      fontWeight: FontWeight.w700),
+                ),
+                Expanded(child: Container()),
+                Text(
+                  "Details",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageDetail,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(
+                  Icons.arrow_forward,
+                  size: 20,
+                  color: color.AppColor.homePageIcons,
                 )
-                //Isha start from here
               ],
+            ),
+            //Isha start from here
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  color.AppColor.gradientFirst.withOpacity(0.8),
+                  color.AppColor.gradientSecond.withOpacity(0.9),
+                ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(80),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(5, 10),
+                    blurRadius: 20,
+                    color: color.AppColor.gradientSecond.withOpacity(0.2),
+                  )
+                ],
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  top: 25,
+                  right: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Next Workout',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: color.AppColor.homePageContainerTextSmall),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Legs Toning',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: color.AppColor.homePageContainerTextSmall),
+                    ),
+                    Text(
+                      'and Glutes Workout',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: color.AppColor.homePageContainerTextSmall),
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: color.AppColor.homePageContainerTextSmall,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '60 min',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: color
+                                      .AppColor.homePageContainerTextSmall),
+                            ),
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: color.AppColor.gradientFirst,
+                                  blurRadius: 10,
+                                  offset: Offset(4, 8))
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.play_circle_fill,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
